668723a3dca7f8ab454c2441e998bcfadc8daa51
starmambwe <starmambwe@gmail.com>
2025-03-25 14:02:17 +0200

    Added UserSeeder.php under /database/seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Insert a single user
+        User::create
+        (
+            [
+                'name' => 'Star Mambwe',
+                'email' => 'starmambwe@gmail.com',
+                'password' => bcrypt('<Qwerty@1234!>'), // Ensure password is hashed
+            ]
+        );
+
+        // You can also create multiple users using a loop or factory
+        // Example for multiple users:
+        // User::factory(10)->create(); // This will create 10 users using the factory
+    }
+}
